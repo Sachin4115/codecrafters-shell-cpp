4d157f9114fa08c4d1a1a3b3844e900a5fc6fe6d
Sachin4115 <sachdeva.4115@gmail.com>
2025-03-27 22:53:33 +0530

    checking

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main() {
     string input;
     getline(std::cin, input);
     if(input=="exit 0") break;
-    else if(input.substr(0,4)=="echo") cout<<input.substr(5);
+    else if(input.substr(0,4)=="echo") cout<<input.substr(5)<<endl;
     else cout << input << ": command not found" <<endl;
   }
 }
