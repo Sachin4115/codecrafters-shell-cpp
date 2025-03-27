bf40cac1bb8c8caa97a322f30b5d8b8e0b4f3d47
Sachin4115 <sachdeva.4115@gmail.com>
2025-03-27 16:27:58 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-
   std::string input;
   while(true){
+    std::cout << "$ ";
     std::getline(std::cin, input);
     std::cout << input << ": command not found" <<std::endl;
   }
