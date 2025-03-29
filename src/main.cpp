fe4408dd065c34ee5ec34ff89bfe60a8957d0cd1
Sachin4115 <sachdeva.4115@gmail.com>
2025-03-30 03:43:15 +0530

    10

--- src/main.cpp
+++ src/main.cpp
@@ -104,8 +104,13 @@ int main() {
       if(command_vector[0]=="cat"){
         string command_with_full_path = "";
         for(int argument_number = 0; argument_number < command_vector.size(); argument_number++){
-          command_with_full_path += " ";
-          command_with_full_path += command_vector[argument_number];
+          if(argument_number!=0)
+            command_with_full_path += " ";
+          string s="";
+          for(char c:command_vector[argument_number])
+            if(c!=' ')
+              s+=c;
+          command_with_full_path += s;
         }
         const char* command_ptr = command_with_full_path.c_str();
         system(command_ptr);
